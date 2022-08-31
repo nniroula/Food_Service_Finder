fa71dfc223d17f82ff5b3a51137d130d96f6ae74
nabin <nkbhaie2017@gmail.com>
2022-08-31 17:51:34 -0600

    add logic so that data from the external api could be retrieved based on the user input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template  #, redirect, request 
+from flask import Flask, render_template, request   #, redirect
 # for debug toolbar
 from flask_debugtoolbar import DebugToolbarExtension
 import requests
@@ -30,31 +30,41 @@ def landing_page():
 api_key = API_SECRET_KEY
 
 BASE_URL = 'https://api.yelp.com/v3'
-BUSINESS_ENDPOINT = 'businesses/search'
-# NEED_API_URL =  'https://api.yelp.com/v3/businesses/search'
+BUSINESS_ENDPOINT = '/businesses/search'
+
+@app.route('/')
+def home():
+    return render_template('city-form.html')
 
 
+@app.route('/city')
+def show_all_food_service_providers_from_api():
+    """ Take in user input of the city and render all food stores from the external api in the browser based on the user input """
 
-# working version
-API_URL =  'https://api.yelp.com/v3/businesses/search?location=Denver' #hard coded location
-# Working version
-headers= {"Authorization": f"Bearer {api_key}" }
+    headers= {"Authorization": f"Bearer {api_key}" }
+    city_name = request.args['city']  # not requests, get user input from the browser
 
-#  working version
-api_response = requests.get(API_URL, headers= headers)
+    # API_URL =  f'https://api.yelp.com/v3/businesses/search?location={city_name}'
+    NEEDED_API_URL =  f'{BASE_URL}{BUSINESS_ENDPOINT}?location={city_name}'
 
-api_data = api_response.json() # returns data in json format
+    api_response = requests.get(NEEDED_API_URL, headers= headers)
+    api_data = api_response.json() # returns data in json format
+    data_object = api_data['businesses']
 
-data_object = api_data['businesses']
-# print(data_object[0])
+    for val in data_object:
+        print('#################################################################')
+        print(val['name'])
+        print(val['is_closed'])
+        print(val['display_phone'])
+        address = val['location'][ 'display_address'] # returns an array(list)
+        address_to_string = ' '.join(address)
+        print(address_to_string)
+        print(val['rating'])
+
+    return render_template('food_providers.html', data_object = data_object)
 
 
-for val in data_object:
-    print('#################################################################')
-    print(val['name'])
-    print(val['is_closed'])
-    print(val['display_phone'])
-    address = val['location'][ 'display_address'] # returns an array(list)
-    address_to_string = ' '.join(address)
-    print(address_to_string)
+################################################################################
+
+
    
