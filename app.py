f7b45c9cc152023cf178f0a90a06491e4a5fe8bf
nabin <nkbhaie2017@gmail.com>
2022-08-30 23:38:20 -0600

    modify some code

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template  #, redirect, request 
+# for debug toolbar
+from flask_debugtoolbar import DebugToolbarExtension
 
-app = Flask(__name__)
 
-@app.route('/')
+app = Flask(__name__)  
+
+#  flask debugtoolbar setup
+app.config['SECRET_KEY'] = "nosecretkeyhere"
+debug = DebugToolbarExtension(app)
+
+@app.route('/first')
 def base_route():
-    return "Started Capstone Project"
+    # return "Started Capstone Project"
+    return render_template('index.html')
+
+# @app.route('/signup')
+# def signup():
+#     return render_template('landing_page.html')
+
+@app.route('/landing')
+def landing_page():
+    return render_template('landing_page.html')
