b8b59d6c63f3f1245b5c58f6b4434f3503c3ecc8
nabin <nkbhaie2017@gmail.com>
2022-09-01 11:32:56 -0600

    refactor the code

--- app.py
+++ app.py
@@ -32,15 +32,12 @@ import random
 def generate_random_list_of_items(arrayOfItems):
     """ generates a list of 12 randomly selected items from a list """
 
-    # length_of_arrayOfItems = len(arrayOfItems)
-
     list_of_twelve_random_items = random.sample(arrayOfItems, 12)
     return list_of_twelve_random_items
 
 
-
 ######################################################################################################
-""" External API Access and data retrieval """
+""" External API Access and data retrieval as well as manipulation """
 
 api_key = API_SECRET_KEY
 BASE_URL = 'https://api.yelp.com/v3'
@@ -55,7 +52,6 @@ def home():
 def show_food_service_providers_from_api():
     """ Take in user input of the city and render all food stores from the external api in the browser based on the user input """
 
-    """ """
     headers= {"Authorization": f"Bearer {api_key}" }
     city_name = request.args['city']  # not requests, get user input from the browser
 
@@ -70,16 +66,7 @@ def show_food_service_providers_from_api():
     other_stores_to_explore = []
 
     for store in api_store_lists_object:
-        print('#################################################################')
-        print(store['name'])
-        print(store['is_closed'])
-        print(store['display_phone'])
-        address = store['location'][ 'display_address'] # returns an array(list)
-        address_to_string = ' '.join(address)
-        print(address_to_string)
-        print(store['rating'])
-
-        if(store['rating'] >= 5.5):
+        if(store['rating'] >= 2.5):
             suggested_stores.append(store['name'])
         else:
             other_stores_to_explore.append(store['name'])
@@ -87,25 +74,20 @@ def show_food_service_providers_from_api():
     length_of_suggested_stores = len(suggested_stores)
     length_of_other_stores = len(other_stores_to_explore)
 
-    ######################################################
-    """ use random geneator function """
-
-    # generate 12 random restaurants
+    # use random geneator function, generate 12 random restaurants
     if(length_of_suggested_stores > 12):
         list_of_randomly_selected_suggested_restaurants = generate_random_list_of_items(suggested_stores)
-        #  empty the suggested stores array, and append the new values
+        #  empty the suggested stores array, and append new values to it
         suggested_stores.clear()
         for restaurant in list_of_randomly_selected_suggested_restaurants:
             suggested_stores.append(restaurant)
     
     if(length_of_other_stores >12):
         list_of_randomly_selected_other_restaurants = generate_random_list_of_items(other_stores_to_explore)
-         #  empty the suggested stores array, and append the new values
+         #  empty the other stores array, and append new values to it
         other_stores_to_explore.clear()
         for restaurant in list_of_randomly_selected_other_restaurants:
             other_stores_to_explore.append(restaurant)
-
-    ######################################################
 
     return render_template('food_providers.html', 
                             suggested_stores = suggested_stores, 
@@ -118,7 +100,6 @@ def show_food_service_providers_from_api():
 ################################################################################
 
 
-   
 # headers= {"Authorization": f"Bearer {api_key}" }
 # NEEDED_API_URL = f'{BASE_URL}{BUSINESS_ENDPOINT}?location=Denver&term=Restaurant'
 
