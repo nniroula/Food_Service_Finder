2c40e4f6ca455f56d790ae84fec2a4aeabe2aec4
nabin <nkbhaie2017@gmail.com>
2022-09-09 07:04:34 -0600

    add unit tests to test a random list generator function

--- test_random_list_generator.py
+++ test_random_list_generator.py
@@ -13,6 +13,7 @@ class RandomListGeneratorTestCase(TestCase):
     """ Testing random list generator functions for displaying 12 random stores at maximum at a time. """
 
     list1 = ['JS', 'Python', 'Ruby', 'C#', 'C++', 'Elixir', 'Cobolt', 'Scala', 'Java', 'NodeJs', 'Fortran', 'Visual Basic']
+    list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
     def test_random_list_generator(self):
         """ Test if two lists are equal. """
@@ -20,6 +21,13 @@ class RandomListGeneratorTestCase(TestCase):
         expected = sorted(self.list1)
         actual = sorted(generate_random_list_of_items(self.list1))
         self.assertListEqual(actual, expected)
+
+    def test_length_of_resultant_list_after_random_generation_is_twelve(self):
+        """ check if the length of the resultant list after passing a list with length greater than 12 through a 
+        random list generator function is 12. """
+
+        actual = generate_random_list_of_items(self.list2)
+        self.assertEqual(len(actual), 12)
 
 
         
